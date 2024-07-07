7f4fa5e355b54b4d503cb894e2b02d32a8924cc7
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-07-07 13:48:03 -0400

    Added more functionality
    Halfway there to do list:
    Polish the app
    Add win/lose functionality
    Aesthetics

--- Connect4.py
+++ Connect4.py
@@ -3,13 +3,13 @@ import tkinter as tk
 class Connect:
     def __init__(self) -> None:
         self.dimensions = (10, 10)
-        self.board = [0 for _ in range(self.dimensions[0]*self.dimensions[1])]
+        self.board = [0 for i in range(self.dimensions[0]*self.dimensions[1])]
         self.CONNECT = 4 # how many in a row
-        self.Player = True # boolean variable; represents player to move
+        self.Player = 1 # represents player to move
         self.draw_grid = True # condition responsible for drawing the grid on screen
         
         self.SIDE_LEN = 500 # in pixels
-        self.TILE_SIZE = self.SIDE_LEN//self.dimensions # also in pixels
+        self.TILE_SIZE = self.SIDE_LEN//self.dimensions[0] # also in pixels
         self.LINE_WID = 1 # width of the lines on the grid in pixels
 
         # Create a new window in tkinter
@@ -22,19 +22,22 @@ class Connect:
         self.game_canvas.config(width=self.SIDE_LEN, height=self.SIDE_LEN)
         # title of the window
         self.root.title("Connect 4")
+        
+    def gameloop(self):
+        while True:
+            self.drawGUI()
+            Column = int(input())%self.dimensions[0] - 1
+            self.board[abs((self.place_piece(Column, self.Player) * self.dimensions[0] + abs(Column - self.dimensions[0])) - len(self.board))] = 1 if self.Player else 2
 
     def place_piece(self, column, player):
         # some code snippets for placing a piece at designated square
-        column%=self.dimensions[1]
         # check for empty column, if so, then find the lowest point that is available
-        for row in range(1, self.dimensions[0]+1):
-            if self.board[(row-1)*self.dimensions[1] + column]!=0:
-                if not row: raise SystemError("invalid move")
-                else: self.board[(row-1)*self.dimensions[1] + column]=player;break
-            if row == self.dimensions[0]:
-                self.board[(row-1)*self.dimensions[1] + column]=player
-        return self.board
-    
+        col = [item for index, item in enumerate(self.board) if index % self.dimensions[0] == column]
+        if sum(col) == len(col):
+            return -1
+        self.Player = not self.Player
+        return col[::-1].index(0)
+
     def drawGUI(self):
         self.game_canvas.delete("all")
         if self.draw_grid:
@@ -44,7 +47,7 @@ class Connect:
             for line in range(self.SIDE_LEN//self.TILE_SIZE):
                 # create the horizontal lines
                 self.game_canvas.create_line(0, line * self.TILE_SIZE, self.SIDE_LEN, line*self.TILE_SIZE, width = self.LINE_WID)
-        for index, item in enumerate(self.pad_board(self.board)):
+        for index, item in enumerate(self.board):
             ind_col, ind_row = index // (self.SIDE_LEN//self.TILE_SIZE), index % (self.SIDE_LEN//self.TILE_SIZE)
             if item:
                 self.game_canvas.create_rectangle((ind_row) * self.TILE_SIZE, (ind_col) * self.TILE_SIZE, (ind_row + 1) * self.TILE_SIZE, (ind_col + 1) * self.TILE_SIZE, fill = self.PlayerColors(item))
@@ -52,7 +55,14 @@ class Connect:
         self.game_canvas.update()
         self.game_canvas.pack()
 
-    def PlayerColors(player):
-        pass
+    def PlayerColors(self, player):
+        player_colors = {
+            #1:"#FF5733",
+            #2:"#E6D735",
+            1:"#E61F00",
+            2:"#E6AE00",
+        }
+        return player_colors[player]
 
 connect = Connect()
+connect.gameloop()
